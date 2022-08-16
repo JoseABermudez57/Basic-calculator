9756d1e3dc150d794701f724669aabdad616fd9e
Zukko <jose.apep570@gmail.com>
2022-08-16 01:49:05 -0500

    Updated 'obtainButtonValues()' function

--- Resources/Javascript/function.js
+++ Resources/Javascript/function.js
@@ -1,7 +1,32 @@
-function obtainButtonValues(val){
+x = "0"; // Number in displayContent
+xi = true; // If a number is in the displayContent
+decPoint = false; // decimal point state
 
-    var buttonValue = val.innerText;
-    console.log(buttonValue);
+function obtainButtonValues(xx) {
+    console.log(xx);
 
-    document.getElementById("display").innerText = buttonValue;
+    if (x == "0" || xi == true) {	// initialize a number
+        document.getElementById("display").innerText = xx; //Show in displayContent
+        x = xx; // Save number
+        if (xx == ".") { // If write a decPoint at the number start
+            document.getElementById("display").innerText = "0."; // Write 0.
+            x = xx; // Save number
+            decPoint = true;
+        }
+    } else { // Continue writing numbers
+        if (xx == "." && decPoint == false) { // If write a decPoint for the first time
+            document.getElementById("display").innerText += xx;
+            x += xx;
+            decPoint = true; // Change the state for the decPoint
+        }
+        // If we write a second decPoint do nothing
+        else if (xx == "." && decPoint == true) { }
+
+        // Other cases (numbers 0 to 9)
+        else {
+            document.getElementById("display").innerText += xx;
+            x += xx
+        }
+    }
+    xi = false // The number was initialized and we can write more numbers
 }
