70ba94f37c69130999f0a8e80a8c55b93527c833
Zukko <jose.apep570@gmail.com>
2022-08-19 22:23:26 -0500

    Bugs Fixed

--- Resources/Javascript/function.js
+++ Resources/Javascript/function.js
@@ -1,21 +1,30 @@
 x = "0"; // Number in displayContent
 xi = true; // If a number is in the displayContent
 decPoint = false; // decimal point state
+op = "" // Obtain the operation
+pn = 0 // Previous number
 
-function obtainButtonValues(xx) {
+window.onload = function () { //Acciones tras cargar la p&aacute;gina
+    display = document.getElementById("display"); //elemento pantalla de salida
+}
+
+function obtainButtonValues(e) {
+    
+    xx = e.innerHTML;
+
     console.log(xx);
 
     if (x == "0" || xi == true) {	// initialize a number
-        document.getElementById("display").innerText = xx; //Show in displayContent
+        display.innerHTML = xx; //Show in displayContent
         x = xx; // Save number
         if (xx == ".") { // If write a decPoint at the number start
-            document.getElementById("display").innerText = "0."; // Write 0.
+            display.innerHTML = "0."; // Write 0.
             x = xx; // Save number
             decPoint = true;
         }
     } else { // Continue writing numbers
         if (xx == "." && decPoint == false) { // If write a decPoint for the first time
-            document.getElementById("display").innerText += xx;
+            display.innerHTML += xx;
             x += xx;
             decPoint = true; // Change the state for the decPoint
         }
@@ -24,9 +33,32 @@ function obtainButtonValues(xx) {
 
         // Other cases (numbers 0 to 9)
         else {
-            document.getElementById("display").innerText += xx;
+            display.innerHTML += xx;
             x += xx
+            console.log(x);
         }
     }
     xi = false // The number was initialized and we can write more numbers
+}
+
+function operate(ope) {
+    obtainResult() //si hay operaciones pendientes se realizan primero
+    pn = x //ponemos el 1º n&uacute;mero en "numero en espera" para poder escribir el segundo.
+    op = ope; //guardamos tipo de operaci&oacute;n.
+    xi = true; //inicializar pantalla.
+}
+
+function obtainResult() {
+    if (op == "") { //no hay ninguna operaci&oacute;n pendiente.
+        console.log(x);
+        display.innerHTML = x;	//
+    }
+    else { //con operaci&oacute;n pendiente resolvemos
+        sl = pn + op + x; // escribimos la operaci&oacute;n en una cadena
+        sol = eval(sl) //convertimos la cadena a c&oacute;digo y resolvemos
+        display.innerHTML = sol //mostramos la soludi&oacute;n
+        x = sol; //guardamos la soluci&oacute;n
+        op = ""; //ya no hayn operaciones pendientes
+        xi = true; //se puede reiniciar la pantalla.
+    }
 }
